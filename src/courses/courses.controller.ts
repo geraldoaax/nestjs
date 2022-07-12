46d29e79ed2e2dc183ed2900dfb3b3fdb9c6289a
Geraldo Dutra Neto <geraldoaax@gmail.com>
2022-07-12 18:46:06 -0300

    services x controllers

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,36 +1,40 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { CoursesService } from './courses.service';
 
 @Controller('courses') //Rota principal --pode definir no metodo
 export class CoursesController {
-  constructor(private readonly coursesService: CoursesService) {
-  }
+  constructor(private readonly coursesService: CoursesService) {}
 
-
-  @Get() //rota aninhada -- parametro
-  findAll(@Res() response) {
-    return response.status(200).send('Listagem de Cursos')
+  @Get()
+  findAll() {
+    return this.coursesService.findAll();
   }
 
   @Get(':id') //rota aninhada -- parametro
-  findOne(@Param('id') id: string) { //desestruturacao
-    return `Curso de ${id}`
+  findOne(@Param('id') id: string) {
+    return this.coursesService.findOne(id);
   }
 
   @Post()
-  @HttpCode(HttpStatus.NO_CONTENT)
-  create(@Body() body: any) { //pegar uma informacao especifica
-    return body
+  create(@Body() body: any) {
+    return this.coursesService.create(body);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() body: any) {
-    return `Atualização do Curso #${id}`
+    return this.coursesService.update(id, body);
   }
 
-  @Delete(':id') //rota aninhada -- parametro
-  remove(@Param('id') id: string) { //desestruturacao
-    return `Exclusão de ${id}`
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.coursesService.remove(id);
   }
-
 }
