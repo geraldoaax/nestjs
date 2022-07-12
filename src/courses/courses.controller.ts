d5902eff856816dff5d47a266e642a7894dbcacd
Geraldo Dutra Neto <geraldoaax@gmail.com>
2022-07-11 21:00:36 -0300

    crud

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,10 +1,10 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Res } from '@nestjs/common';
 
 @Controller('courses') //Rota principal --pode definir no metodo
 export class CoursesController {
   @Get() //rota aninhada -- parametro
-  findAll() {
-    return 'Listagem de Cursos'
+  findAll(@Res() response) {
+    return response.status(200).send('Listagem de Cursos')
   }
 
   @Get(':id') //rota aninhada -- parametro
@@ -13,7 +13,19 @@ export class CoursesController {
   }
 
   @Post()
-  create(@Body() body: any) {
+  @HttpCode(HttpStatus.NO_CONTENT)
+  create(@Body() body: any) { //pegar uma informacao especifica
     return body
   }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body: any) {
+    return `Atualização do Curso #${id}`
+  }
+
+  @Delete(':id') //rota aninhada -- parametro
+  remove(@Param('id') id: string) { //desestruturacao
+    return `Exclusão de ${id}`
+  }
+
 }
